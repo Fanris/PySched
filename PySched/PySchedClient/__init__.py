adf476bb0bb5c663232cd5b31f074970503367bd
Martin Predki <martin.predki@gmx.net>
2013-05-24 16:27:50 +0200

    version update

--- PySched/PySchedClient/__init__.py
+++ PySched/PySchedClient/__init__.py
@@ -27,7 +27,7 @@ import os
 import logging
 import datetime
 
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 TITLE = """
  _____        _____      _              _  _____ _ _            _    
 |  __ \      / ____|    | |            | |/ ____| (_)          | |   
--- PySched/PySchedServer/__init__.py
+++ PySched/PySchedServer/__init__.py
@@ -30,7 +30,7 @@ import datetime
 import os
 
 
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 TITLE = """
  _____        _____      _              _  _____                           
 |  __ \      / ____|    | |            | |/ ____|                          
