eccb3c399b0418243b2fc0a3f91ff6486a44bf13
Martin Predki <martin.predki@gmx.net>
2013-06-24 12:49:46 +0200

    test

--- PySched/PySchedClient/PySchedClient.sh
+++ PySched/PySchedClient/PySchedClient.sh
@@ -19,7 +19,7 @@ def main():
 
     res = PySchedClient(args.workingDir, args)
     print "PySchedClient terminated with {}".format(res)
-    print "Install path: {}".format(PySchedClient.__file__)
+    print "Install path: {}".format(PySched.PySchedClient.__file__)
 
 if __name__ == '__main__':
     main()
