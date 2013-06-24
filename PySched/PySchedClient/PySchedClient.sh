08f235c4b17e0d4ca5904f26cc87bcf15cc4a5bb
Martin Predki <martin.predki@gmx.net>
2013-06-24 14:41:38 +0200

    bugfix

--- PySched/PySchedClient/PySchedClient.sh
+++ PySched/PySchedClient/PySchedClient.sh
@@ -29,7 +29,7 @@ def main(args=None):
     res = PySchedClient.PySchedClient(args.workingDir, args)
     if res.runUpdate:
         if update(PySchedClient.__file__, args):
-            print *sys.argv[]
+            print *sys.argv
             os.execl(sys.executable, *sys.argv[])
 
 def update(installPath, args):
